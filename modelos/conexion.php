a83c4c3d7c81115407120b4d3f8394ad00e1fabd
Nozen <ramirezedelvalle@gmail.com>
2023-05-18 10:25:09 -0600

    editar

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -3,12 +3,13 @@ abstract class Conexion{
     public static $conexion = null;
 
     private static fuction conectar(){
+    echo "Conectado";
         try{
             self::$conexion = new PDO("informix:host=host.docker.internal; service=9088;database=mdn; server=informix; protocol=onsoctcp;EnableScrollableCursors=1", "informix", "in4mix")
         }catch(PDOException $e){
             echo "Error al conectar: ";
             exit;
         }
-        return self::$conexion
+        return self::$conexion;
     }
 }
